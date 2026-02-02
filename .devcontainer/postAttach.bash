1f618094862a71e4eb021a9cc8ca4649b6b2f577
Grant Braught <braught@dickinson.edu>
2026-02-02 18:50:42 +0000

    autoforwarding

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -1,21 +1,25 @@
 sudo chgrp docker /var/run/docker.sock
 
-docker ps | grep "rev_proxy"
+# docker ps | grep "rev_proxy"
 
-PROXY_RUNNING=$(docker ps | grep "rev_proxy")
-echo "$PROXY_RUNNING"
+# PROXY_RUNNING=$(docker ps | grep "rev_proxy")
+# echo "$PROXY_RUNNING"
 
-if [ -z "${PROXY_RUNNING}" ];
-then
-  echo "Starting reverse proxy..."
-  docker start rev_proxy
-  echo "Started."
-fi
+# if [ -z "${PROXY_RUNNING}" ];
+# then
+#   echo "Starting reverse proxy..."
+#   docker start rev_proxy
+#   echo "Started."
+# fi
 
-echo "All ready to go!"
+
+
 PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
 echo "Proxy is running on port ${PROXY_PORT}."
+
+echo "Proxy is using: https://localhost:${PROXY_PORT}"
 
 URL="https://${CODESPACE_NAME}-${PROXY_PORT}.app.github.dev"
 echo "URL for reverse proxy is: ${URL}"
 
+echo "All ready to go!"
