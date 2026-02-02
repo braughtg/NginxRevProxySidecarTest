a48b59ba563ca7ce2c61b85b7d6917ba87b90038
Grant Braught <braught@dickinson.edu>
2026-02-02 17:19:05 +0000

    add port info in output

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -1,3 +1,8 @@
 sudo chgrp docker /var/run/docker.sock
 
 echo "All ready to go!"
+PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
+echo "Proxy is running on port $PROXY_PORT."
+URL="https://${CODESPACE_NAME}-${PROXY_PORT}.app.github.dev"
+echo "Click here to open: $URL"
+
