b061fb71f265be808f7ce992ed4b323456897365
Grant Braught <braught@dickinson.edu>
2026-02-02 18:28:16 +0000

    add restart for proxy

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -1,8 +1,21 @@
 sudo chgrp docker /var/run/docker.sock
 
+docker ps | grep "rev_proxy"
+
+PROXY_RUNNING=$(docker ps | grep "rev_proxy")
+echo "$PROXY_RUNNING"
+
+if [ -z "${PROXY_RUNNING}" ];
+then
+  echo "Starting reverse proxy..."
+  docker start rev_proxy
+  echo "Started."
+fi
+
 echo "All ready to go!"
 PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
-echo "Proxy is running on port $PROXY_PORT."
+echo "Proxy is running on port ${PROXY_PORT}."
+
 URL="https://${CODESPACE_NAME}-${PROXY_PORT}.app.github.dev"
-echo "Click here to open: $URL"
+echo "URL for reverse proxy is: ${URL}"
 
