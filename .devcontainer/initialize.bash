f8a9e6f3d68d54cbb56a17a23e8dc04643f09b74
Grant Braught <braught@dickinson.edu>
2026-02-01 19:17:45 -0500

    Comment out PID retrieval and kill commands
    Comment out the code that retrieves and kills the PID for the codespace process on port 443.

--- .devcontainer/initialize.bash
+++ .devcontainer/initialize.bash
@@ -18,11 +18,11 @@ echo "Checking for docker-pr..."
 sudo lsof -i :443 | grep "docker-pr.*\*:https"
 echo "Checking for codespace..."
 sudo lsof -i :443 | grep "codespace.*\*:https"
-PID=$(sudo lsof -i :443 | grep "codespace.*\*:https" | cut -d' ' -f2)
-echo "PID=$PID"
-echo "Killing PID=$PID..."
-sudo kill -9 "$PID"
-echo "Dead."
+#PID=$(sudo lsof -i :443 | grep "codespace.*\*:https" | cut -d' ' -f2)
+#echo "PID=$PID"
+#echo "Killing PID=$PID..."
+#sudo kill -9 "$PID"
+#echo "Dead."
 echo "Checking all again..."
 sudo lsof -i :443
 echo ""
