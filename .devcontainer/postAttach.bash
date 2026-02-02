2def56f20c96857e7ddd69c5d88710532fa08bbf
Grant Braught <braught@dickinson.edu>
2026-02-02 19:23:13 +0000

    seems to be working

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -1,25 +1,9 @@
 sudo chgrp docker /var/run/docker.sock
 
-# docker ps | grep "rev_proxy"
-
-# PROXY_RUNNING=$(docker ps | grep "rev_proxy")
-# echo "$PROXY_RUNNING"
-
-# if [ -z "${PROXY_RUNNING}" ];
-# then
-#   echo "Starting reverse proxy..."
-#   docker start rev_proxy
-#   echo "Started."
-# fi
-
-
 
 PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
-echo "Proxy is running on port ${PROXY_PORT}."
-
 echo "Proxy is using: https://localhost:${PROXY_PORT}"
 
-URL="https://${CODESPACE_NAME}-${PROXY_PORT}.app.github.dev"
-echo "URL for reverse proxy is: ${URL}"
+echo "noVNC is using: http://localhost:6901"
 
 echo "All ready to go!"
