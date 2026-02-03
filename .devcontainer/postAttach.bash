45fb482379f1128ab5e7860910422ba745fbd3f5
Grant Braught <braught@dickinson.edu>
2026-02-03 00:13:35 +0000

    working in codespace

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -1,8 +1,6 @@
 sudo chgrp docker /var/run/docker.sock
 
-
-#PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
-#echo "Proxy is using: https://localhost:${PROXY_PORT}"
-#echo "noVNC is using: http://localhost:6901"
+PROXY_PORT=$(docker port rev_proxy | cut -d':' -f2 | head -1)
+echo "Proxy is using: https://localhost:${PROXY_PORT}"
 
 echo "All ready to go!"
